6bfc5b2e34dd2c5f24e27f05a303bfa9639b8cce
mohsin324 <mohsinmunir324@gmail.com>
2022-09-20 23:24:27 +0500

    completed signUp Form

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
-const btn = document.getElementById('btn');
-console.log(btn);
-const cnic = document.getElementById('cnic');
+// getting values into Dom
+// firstName
+const firstName = document.getElementById('firstName');
+// console.log(firstName);
+//lastName
+const lastName = document.getElementById('lastName');
+// console.log(lastName);
+//email
 const email = document.getElementById('email');
+// console.log(email);
+//password
 const password = document.getElementById('password');
-const small1 = document.getElementById('small1');
-console.log(small1)
-btn.addEventListener('click', (e)=>{
-    // e.preventDefault();
-    if(cnic.value != null && email.value != null && password.value != null){
-        small1.style.display = 'block'
-        console.log(cnic.value);
+// console.log(password);
+//confirmPassword
+const confirmPassword = document.getElementById('confirmPassword');
+// console.log(confirmPassword);
+const form = document.getElementById('form');
+console.log(form);
+//show error message
+function showError(input, message){
+    const formControl = input.parentElement;
+    formControl.className = 'firstName error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+//show success 
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className = 'firstName success';
+}
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    if(firstName.value == ''){
+       showError(firstName, 'firstName is required');
     }else{
-        console.log('there is no value')
+        showSuccess(firstName)
     }
+    if(lastName.value == ''){
+        showError(lastName, 'lastName is required');
+     }else{
+         showSuccess(lastName)
+     }
+     if(email.value == ''){
+        showError(email, 'email is required');
+     }else{
+         showSuccess(email)
+     }
+     if(password.value == ''){
+        showError(password, 'password is required');
+     }else{
+         showSuccess(password)
+     }
+     if(confirmPassword.value !== password.value){
+        showError(confirmPassword, 'password not matched');
+     }else{
+         showSuccess(confirmPassword)
+     }
 
 })
-// small.innerHtml = "Hello World"
